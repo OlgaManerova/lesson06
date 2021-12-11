5cc9104494900ec7583b2351b30d1b6b28dcdc71
vovchik3003 <vovchik3003@mail.ru>
2021-12-11 22:24:25 +0300

    Задание 11 урока

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const button = document.querySelector('#btn');
+const input = document.querySelector('#text');
+const square = document.querySelector('#square');
+const e_btn = document.querySelector('#e_btn');
+const range = document.querySelector('#range');
+const rangeSpan = document.querySelector('#range-span');
+
+
+button.addEventListener('click', function() {
+    square.style.backgroundColor = input.value;
+})
+
+e_btn.style.display = 'none';
+
+range.addEventListener('input', function() {
+    rangeSpan.textContent = range.value;
+    circle.style.width = range.value + 'px';
+    circle.style.height = range.value + 'px';
+})
