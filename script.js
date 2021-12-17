bd96fbeff007dcd24fc4f0637a070a5e5796b1dc
Manerova Olga <o.manerova@qsoft.ru>
2021-12-17 18:21:43 +0300

    задание 15 урока

--- script.js
+++ script.js
@@ -11,12 +11,12 @@ const DomElement = function(selector, height, width, bg, fontSize) {
 }
 
 DomElement.prototype.createEl = function() {
-    if (selector.charAt(0) == '.') {
+    if (this.selector.charAt(0) == '.') {
         const div = document.createElement('div');
         div.classList.add('class');
         div.innerHTML = 'Создали div';
         block.append(div);
-    } else if (selector.charAt(0) == '#') {
+    } else if (this.selector.charAt(0) == '#') {
         const p = document.createElement('p');
         p.classList.add('id');
         p.innerHTML = 'Создали p';
@@ -25,5 +25,7 @@ DomElement.prototype.createEl = function() {
 }
 
 const DomElement1 = new DomElement('.block');
+const DomElement2 = new DomElement('#best');
 
 DomElement1.createEl();
+DomElement2.createEl();
